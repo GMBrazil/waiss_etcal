80e3c5e236690a0b15f9cf3314c1e1c4e45db958
GMBrazil <56103857+GMBrazil@users.noreply.github.com>
2021-02-19 10:10:53 +0800

    edited NewUserForm in forms

--- waiss_etcal/etcal/forms.py
+++ waiss_etcal/etcal/forms.py
@@ -8,13 +8,4 @@ class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "username", "email", "password1", "password2")
-
-    def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+        fields = ("first_name", "last_name", "username", "email", "password1", "password2",)
