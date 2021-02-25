d2feb91e016e753f04d00afb399827804653bdd6
GMBrazil <ginalyn05brazil@gmail.com>
2021-02-25 11:00:45 +0800

    put etcal app first in list for first rendering

--- waiss_etcal/waiss_etcal/settings.py
+++ waiss_etcal/waiss_etcal/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['waisset.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'etcal.apps.EtcalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'etcal.apps.EtcalConfig',
     "crispy_forms",
 ]
 
