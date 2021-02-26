4e4b7251b9b08989e3cd4e1b865b9e2a58022cd0
GMBrazil <56103857+GMBrazil@users.noreply.github.com>
2021-02-26 13:24:08 +0800

    edited data non input

--- waiss_etcal/etcal/views.py
+++ waiss_etcal/etcal/views.py
@@ -139,8 +139,13 @@ def get_started(request):
                 if (date != "") and (eto != "") and ((rainfall == "") or (irrigation == "")):
                     rainfall = 0
                     irrigation = 0
-                data, created = Data.objects.get_or_create(farm=farm, station=station, timestamp=date, eto=eto, rainfall=rainfall, irrigation=irrigation)
-                data.save()
+                    data, created = Data.objects.get_or_create(farm=farm, station=station, timestamp=date, eto=eto, rainfall=rainfall, irrigation=irrigation)
+                    data.save()
+                elif (date == "") and (eto == "") and ((rainfall == "") or (irrigation == "")):
+                    date = None
+                    eto = None
+                    rainfall = None
+                    irrigation = None
             return HttpResponseRedirect(reverse('etcal:dashboard'))
         elif (date_measured == "") or (eto_data == "") or (rain_data == "") or (irrig_data == ""):
             return HttpResponseRedirect(reverse('etcal:dashboard'))
