e4d56fd93514ffcb3688dd32b095660fd2470508
GMBrazil <56103857+GMBrazil@users.noreply.github.com>
2021-02-26 16:40:47 +0800

    changed name of button

--- waiss_etcal/etcal/static/dash/js/script.js
+++ waiss_etcal/etcal/static/dash/js/script.js
@@ -129,7 +129,7 @@ Highcharts.ajax({
                     positioner: function () {
                         return {
                             // right aligned
-                            x: this.chart.chartWidth - this.label.width - this.exporting.buttons.contextButton.width ,
+                            x: this.chart.chartWidth - this.label.width - this.contextButton.width ,
                             y: 10 // align to title
                         };
                     },
