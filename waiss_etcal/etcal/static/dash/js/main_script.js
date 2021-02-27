bbec3c51d3510503863b51c12a9b3d96d947047c
GMBrazil <ginalyn05brazil@gmail.com>
2021-02-27 17:53:25 +0800

    gauge meter

--- waiss_etcal/etcal/static/dash/js/main_script.js
+++ waiss_etcal/etcal/static/dash/js/main_script.js
@@ -365,6 +365,24 @@ function soilWaterStatus() {
         document.getElementById("valActualRAW").textContent = diffActualRAW + "mm near PWP";
         document.getElementById("textSoilWaterNote").textContent = "The current soil moisture content is " + valMC + "mm" + " which is in critical condition with" + diffActualRAW + "mm of soil moisture near permanent wilting point.";
     }
+    var dial = $(".dial .inner");
+    var gauge_value = $(".gauge .value");
+
+    function rotateDial() {
+        var deg = 0;
+        var value = (currentpercentMC * 100).toFixed(2);
+        deg = (value * 177.5) / 100;
+
+        gauge_value.html(value + "%");
+
+        dial.css({ 'transform': 'rotate(' + deg + 'deg)' });
+        dial.css({ '-ms-transform': 'rotate(' + deg + 'deg)' });
+        dial.css({ '-moz-transform': 'rotate(' + deg + 'deg)' });
+        dial.css({ '-o-transform': 'rotate(' + deg + 'deg)' });
+        dial.css({ '-webkit-transform': 'rotate(' + deg + 'deg)' });
+    }
+
+    setInterval(rotateDial, 2000);
 }
 
 function dayToIrrigate() {
