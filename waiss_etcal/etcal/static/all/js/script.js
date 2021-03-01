a48ce03693391db6bd9f69ce04a7fb76222583a9
GMBrazil <ginalyn05brazil@gmail.com>
2021-03-01 16:41:39 +0800

    navlink

--- waiss_etcal/etcal/static/all/js/script.js
+++ waiss_etcal/etcal/static/all/js/script.js
@@ -1,4 +1,4 @@
 //get current URL path and assign 'active class
 var pathname = window.location.pathname;
 $('.nav > li > ul > a[href="' + pathname + '"]').parent().addClass('active');
-$('.side-navbar > li > a[href="' + pathname + '"]').parent().addClass('active');
+$('.side-navbar > li > a[href="' + pathname + '"]').addClass('active');
